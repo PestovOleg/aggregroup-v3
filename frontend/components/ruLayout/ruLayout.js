ec3c8eeaf0c7819359fc2b1753099c5b6c7e7909
Oleg <pestoff.oleg@gmail.com>
2021-10-14 22:16:55 +0300

    0.0.1

---
+++ frontend/components/ruLayout/ruLayout.js
@@ -0,0 +1,4 @@
+
+export default function RuLayout(){
+    return <div>FUCK OFF</div>
+}
---
+++ frontend/pages/ru/index.js
@@ -0,0 +1,5 @@
+import RuLayout from "../../components/ruLayout/ruLayout"
+
+export default function RuHome(){
+  return <div>FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU</div>
+}
